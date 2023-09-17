d40521655deb89e69efe96c1aeb51367b3804ad1
Shrevi <shrevijainbhl@gmail.com>
2023-09-17 22:11:15 +0530

    added api key, extract temperature and sky information

--- backend.py
+++ backend.py
@@ -1,18 +1,14 @@
 import requests
 
 APIkey = "b43d205a205ca641078f92ad86543ea6"
-def get_data(place, days, kind):
+def get_data(place, days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={APIkey}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     nr_value = 8 * days
     filtered_data = filtered_data[:nr_value]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
     return(filtered_data)
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", days=3, kind='Sky'))
+    print(get_data(place="Tokyo", days=3))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -8,12 +9,23 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2023-13-9", "2023-14-9", "2023-15-9"]
-    temperatures = [10, 11, 18]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
 
-d,t = get_data(days)
-figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+if place:
+    try:
+        filtered_data = get_data(place, days)
+
+        if option == 'Temperature':
+            temperature = [dict['main']['temp'] /10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperature, labels={"x": "Dates", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == 'Sky':
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png","Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_condition = [dict['weather'][0]['main'] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_condition]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.write("That place does not exist")
+
+
